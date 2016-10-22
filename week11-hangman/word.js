a577814ec1e40e34ee14831c387ef7977a792a04
jcpax <jcpax5@gmail.com>
2016-10-22 12:17:23 -0500

    Add files via upload

---
+++ week11-hangman/word.js
@@ -0,0 +1,18 @@
+function Words() {
+	this.wordList = ['cake', 'pies', 'clam', 'couches', 'bewitched', 'fancy', 'mother', 'stranger', 'throw',
+		'scary', 'tasteful', 'bread', 'enchanted', 'cabin', 'fear', 'useless', 'special', 'support', 'protective',
+		'aboard', 'enter', 'unruly', 'quickest', 'steel', 'root', 'frog', 'blot', 'cactus', 'judge', 'yummy', 'ajar',
+		'force', 'french', 'trust', 'enemies', 'follow', 'burst', 'amuse', 'elite', 'reflective', 'wary', 'suspend',
+		'functional', 'light', 'top', 'robin', 'blushing', 'activity', 'representative', 'ludicrous', 'cuddly', 'guide',
+		'system', 'shirt', 'birds', 'addition', 'successful', 'cakes', 'wandering', 'spiffy', 'cumbersome',
+		'run', 'invite', 'club', 'plantaton', 'heat', 'lackadaisical', 'appliance', 'wing', 'black', 'hospital', 'locket',
+		'shaggy', 'sweater', 'territory', 'finger', 'connection', 'ugly', 'needle', 'rent', 'handsome', 'ask', 'house',
+		'nappy', 'stop', 'earthy', 'governor', 'neat', 'smokey', 'believe', 'search', 'rhyme', 'escape', 'hurry', 'attack',
+		'industrious', 'grab', 'jagged', 'common', 'different', 'coil'];
+}
+
+var wordFunctions = new Words();
+
+
+exports.word = {
+	wordFunctions: wordFunctions
